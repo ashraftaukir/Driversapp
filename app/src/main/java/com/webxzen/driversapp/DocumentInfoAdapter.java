bf07ce511558cd70d3dadb4460da0d720c8a248a
ashraftaukir <ashraftaukir@gmail.com>
2017-09-18 17:39:57 +0600

    document page design added

---
+++ app/src/main/java/com/webxzen/driversapp/DocumentInfoAdapter.java
@@ -0,0 +1,48 @@
+package com.webxzen.driversapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class DocumentInfoAdapter extends RecyclerView.Adapter<DocumentInfoAdapter.MyViewHolder> {
+
+    private ArrayList<String> documnetlist;
+
+    public DocumentInfoAdapter(ArrayList<String> documnetlist) {
+        this.documnetlist = documnetlist;
+
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.documents_list, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        holder.documentlistItem.setText(documnetlist.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return documnetlist.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView documentlistItem;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            documentlistItem = (TextView) itemView.findViewById(R.id.documentlistItem);
+        }
+    }
+}
--- app/src/main/java/com/webxzen/driversapp/DocumentsFragment.java
+++ app/src/main/java/com/webxzen/driversapp/DocumentsFragment.java
@@ -5,23 +5,58 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 
 public class DocumentsFragment extends Fragment {
 
-View view;
+    RecyclerView docrecylerview;
+
+    DocumentInfoAdapter docadpater;
+    View view;
+    ArrayList<String> list;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-
-        view=inflater.inflate(R.layout.documents, container, false);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("DOCUMENTS");
-
+        view = inflater.inflate(R.layout.documents, container, false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("DOCUMENTS");
+        addingdocumentsinfo();
+        initialization();
         return view;
+
+    }
+
+    private void addingdocumentsinfo() {
+
+        list = new ArrayList<String>();
+        list.add("Driver's License");
+        list.add("NID Front Side");
+        list.add("Vehicle Certification of Fitness");
+        list.add("Vehicle Tax Token");
+        list.add("Vehicle Registration");
+
+    }
+
+    private void initialization() {
+        docrecylerview = (RecyclerView) view.findViewById(R.id.docrecylerView);
+        docadpater = new DocumentInfoAdapter(list);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        docrecylerview.setLayoutManager(mLayoutManager);
+        DividerItemDecoration   mDividerItemDecoration = new DividerItemDecoration(docrecylerview.getContext(),
+                mLayoutManager.getOrientation());
+        docrecylerview.addItemDecoration(mDividerItemDecoration);
+        docrecylerview.setAdapter(docadpater);
+
 
     }
 
