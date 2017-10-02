15d9963c8fd8071d99e5745884ba8370c541cd85
ashraftaukir <ashraftaukir@gmail.com>
2017-10-02 15:57:04 +0600

    Toast message change and screen orientation has been potrait mode

--- app/src/main/java/com/webxzen/driversapp/login/RegistrationFragment.java
+++ app/src/main/java/com/webxzen/driversapp/login/RegistrationFragment.java
@@ -146,7 +146,7 @@ public class RegistrationFragment extends BaseFragment implements View.OnClickLi
                     if (dialogUtil != null) {
                         dialogUtil.dismissProgress();
                     }
-                    Toast.makeText(getActivity(), "Testing error", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(),getString(R.string.tryagain), Toast.LENGTH_SHORT).show();
                 }
             });
         } else {
